2cfcea1a5e86082b43b3a225f63a6566d98bb596
Dominik Hryshaiev <domhryshaiev@gmail.com>
2024-03-05 12:24:00 +0200

    Submit solution for exercise 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,8 +3,18 @@ import {MDXRemote} from "next-mdx-remote/rsc";
 
 import {loadBlogPost} from "@/helpers/file-helpers";
 import BlogHero from '@/components/BlogHero';
+import {BLOG_TITLE} from "@/constants";
 
 import styles from './postSlug.module.css';
+
+export async function generateMetadata({ params }) {
+  const {frontmatter} = await loadBlogPost(params.postSlug);
+
+  return {
+    title: `${frontmatter.title} | ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const blogPost = await loadBlogPost(params.postSlug);
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,12 @@ import BlogSummaryCard from '@/components/BlogSummaryCard';
 import {getBlogPostList} from "@/helpers/file-helpers";
 
 import styles from './homepage.module.css';
+import {BLOG_TITLE} from "@/constants";
+
+export const metadata = {
+  title: BLOG_TITLE,
+  description: 'A wonderful blog about JavaScript',
+}
 
 async function Home() {
   const blogPosts = await getBlogPostList();
